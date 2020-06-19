92d05092f73b849a56286c24de5f52059e3dd890
HyanSource <s815651@yahoo.com.tw>
2020-06-20 00:39:48 +0800

    property

---
+++ hnet/property.go
@@ -0,0 +1,44 @@
+package hnet
+
+import (
+	"errors"
+
+	"github.com/HyanSource/hyannetserver/hinterface"
+)
+
+type Property struct {
+	propertys map[string]interface{}
+}
+
+func NewProperty() hinterface.IProperty {
+
+	return &Property{
+		propertys: make(map[string]interface{}),
+	}
+}
+
+func (t *Property) SetProperty(key string, value interface{}) {
+	if _, ok := t.propertys[key]; ok {
+		return
+	}
+	t.propertys[key] = value
+}
+
+func (t *Property) GetProperty(key string) (interface{}, error) {
+
+	if _, ok := t.propertys[key]; ok {
+		return t.propertys[key], nil
+	}
+
+	return nil, errors.New("property not found key:" + key)
+}
+
+func (t *Property) RemoveProperty(key string) bool {
+
+	if _, ok := t.propertys[key]; ok {
+		delete(t.propertys, key)
+		return true
+	}
+
+	return false
+}
